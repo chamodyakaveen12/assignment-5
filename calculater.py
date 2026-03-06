e9e68d455dc8594ef7c041043ccc218554c9b813
Themiya <themiyawijenayaka100@gmail.com>
2026-03-06 10:46:16 +0530

    Final Calculator Update

--- calculater.py
+++ calculater.py
@@ -1,8 +1,13 @@
+
+
 def add(a, b):
     return a + b
 
 def multiply(a, b):
     return a * b
 
-print(add(5, 3))
-print(multiply(5, 3))
+x= int (input("Input Number 1: "))
+y= int (input("Input Number 2: "))
+
+print("Sum of Two Numbers:            " , add(x, y))
+print("Multiplication of Two Numbers: " , multiply(x, y))
